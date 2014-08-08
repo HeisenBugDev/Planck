12efab1959c40e1b58865bf3287c1c15efe530bf
hunterboerner <hunterboerner@gmail.com>
2014-08-08 16:53:00 -0500

    init and post init

--- src/main/java/heisenbugdev/planck/Planck.java
+++ src/main/java/heisenbugdev/planck/Planck.java
@@ -1,5 +1,7 @@
 package heisenbugdev.planck;
 
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,15 +10,24 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Planck.Data.ID, name = Planck.Data.NAME, version = "@VERSION@")
 public class Planck {
-	public static class Data {
-		public static final String ID = "planck-framework";
-		public static final String NAME = "Planck Framework";
-		public static final Logger log = LogManager.getLogger("Planck");
-	}
-	
-	@Mod.EventHandler
-	public static void preInit(FMLPreInitializationEvent event) {
-		Data.log.info("Pre-initialzing Planck.");
-	}
-	
+    public static class Data {
+        public static final String ID = "planck-framework";
+        public static final String NAME = "Planck Framework";
+        public static final Logger log = LogManager.getLogger("Planck");
+    }
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {
+        Data.log.info("Pre-initializing Planck.");
+    }
+
+    @Mod.EventHandler
+    public static void init(FMLInitializationEvent event) {
+        Data.log.info("Initializing Planck.");
+    }
+
+    @Mod.EventHandler
+    public static void postInit(FMLPostInitializationEvent event) {
+        Data.log.info("Post-initializing Planck.");
+    }
 }
